82445d529b1721b1e50b5bc6b3caa82b2f9f9f71
Waterseaocean <mizuki171201@gmail.com>
2026-05-24 16:28:13 +0900

    feat: add test command

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const client = new discord.Client({
   intents: Object.values(discord.GatewayIntentBits)
 });
 
-client.on(discord.Events.ClientReady, () => {
+client.on(discord.Events.ClientReady, async () => {
   console.log(`Logged in as ${client.user.tag}!`);
+
+  // コマンドの登録
+  const commands = [
+    new discord.SlashCommandBuilder()
+      .setName('test')
+      .setDescription('test command')
+  ];
+
+  await client.application.commands.set(commands);
 });
 
 client.on(discord.Events.MessageCreate, async (message) => {
@@ -14,5 +23,16 @@ client.on(discord.Events.MessageCreate, async (message) => {
     await message.reply('Hello!');
   }
 })
+
+// コマンドの実行時の挙動
+client.on(discord.Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.commandName;
+
+  if (command === 'test') {
+    await interaction.reply('finished');
+  }
+});
 
 client.login(process.env.DISCORD_BOT_TOKEN);
